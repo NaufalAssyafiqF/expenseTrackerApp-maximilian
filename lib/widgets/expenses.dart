a7d12e0442bd53453004d882cb013944d158496c
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2025-04-19 21:14:25 +0700

    add expanse list n expanse item widget

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,57 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpense = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Burger',
+      amount: 10.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+
+  void _openAddExpenseOverlay() {
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Text('The Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpense)),
+        ],
+      ),
+    );
+  }
+}
