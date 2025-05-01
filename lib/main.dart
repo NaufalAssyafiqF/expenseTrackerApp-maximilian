0932f5e72d34274b8047c9cdbc6a3e0e77f64d19
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2025-05-01 17:08:12 +0700

    add theme to all widget dan dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,51 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 167, 137, 237),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer),
-        cardTheme: CardTheme(
-          color: const Color.fromARGB(255, 203, 181, 255),
-          shadowColor: kColorScheme.shadow,
-          elevation: 4,
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        )),
+        textTheme: TextTheme().copyWith(
+          titleLarge: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 14,
+              color: kDarkColorScheme.onSecondaryContainer),
         ),
       ),
+      theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer),
+          cardTheme: CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          )),
+          textTheme: TextTheme().copyWith(
+            titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 14,
+                color: kColorScheme.onSecondaryContainer),
+          )),
       home: Expenses(),
     ),
   );
